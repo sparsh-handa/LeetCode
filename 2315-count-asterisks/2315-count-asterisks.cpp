99dd1c053fb454cf0758e1d0f465e502e7740c29
Sparsh Handa <sparshhanda2002@gmail.com>
2023-06-19 00:52:10 +0530

    Time: 5 ms (42.24%), Space: 6.4 MB (33.53%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -1,12 +1,31 @@
+// class Solution {
+// public:
+//     int countAsterisks(string s) {
+//         int ans = 0;
+//         int brace = 0;
+//         for(int i=0;i<s.length();i++){
+//             if(s[i]=='|' && brace!=1) brace=1;
+//             else if(s[i]=='*' && brace!=1) ans++;
+//             else if(s[i]=='|' && brace==1) brace=0;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int countAsterisks(string s) {
-        int ans = 0;
-        int brace = 0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='|' && brace!=1) brace=1;
-            else if(s[i]=='*' && brace!=1) ans++;
-            else if(s[i]=='|' && brace==1) brace=0;
+	
+	 int ans=0;
+	 
+	// count of no. of '|'
+        int cnt=0;
+		
+        for(int i=0;i<s.size() ;i++) {
+		
+              if(cnt%2==0) {
+                  if(s[i]=='*') ans++;
+              }
+              if(s[i]=='|') cnt++;
         }
         return ans;
     }
