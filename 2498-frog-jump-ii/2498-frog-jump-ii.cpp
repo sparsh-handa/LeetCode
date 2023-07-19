c0bc089f1364a4ed4b69ea3584b1b9f301683ff6
Sparsh Handa <sparshhanda2002@gmail.com>
2023-07-19 22:30:58 +0530

    Time: 167 ms (43.57%), Space: 65.3 MB (53.00%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-int f(int ind,vector<int> &stones,vector<int> &dp){
-    
-    if(ind==0) return 0;
-    if(ind==1) return abs(stones[ind]-stones[ind-1]);
-    if(dp[ind]!=-1) return dp[ind];
-    
-    int curr=abs(stones[ind]-stones[ind-2]);
-    int rem=f(ind-1,stones,dp);
-    
-    return dp[ind]=max(curr,rem);
-}
-
-int maxJump(vector<int>& stones) {
-    
-    int n=stones.size();
-    vector<int> dp(n,-1);
-    return f(n-1,stones,dp);
-}
+    int maxJump(vector<int>& stones) {
+        int a = stones[1] - stones[0];
+        for(int i = 2; i < stones.size(); i++){
+            int ans = stones[i] - stones[i - 1] + stones[i - 1] - stones[i - 2];
+            a = max(a, ans);
+        }
+        return a;
+    }
 };
-
