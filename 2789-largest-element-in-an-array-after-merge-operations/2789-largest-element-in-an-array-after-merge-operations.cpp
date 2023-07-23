9fbf98b2022e6a5756c100d27e835f07f0870347
Sparsh Handa <sparshhanda2002@gmail.com>
2023-07-23 20:24:04 +0530

    Time: 207 ms (58.33%), Space: 105.2 MB (41.67%) - LeetHub

---
+++ 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long maxArrayValue(vector<int>& nums) {
+        int n = nums.size();
+        long long ans  = nums[n-1] , sum = nums[n-1];
+        for(int i= nums.size()-2 ; i>=0 ; i--){
+            if(nums[i] <= sum) sum += nums[i];
+            else sum = nums[i];
+          
+            ans = max(ans,sum);
+        }
+        return ans;
+    }
+};
